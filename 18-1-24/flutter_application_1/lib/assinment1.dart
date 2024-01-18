ff9963644bc001a4752175d88b6d87422103a100
Ankush Rama Bichewar <139443586+ankushbichewar@users.noreply.github.com>
2024-01-18 23:20:23 +0530

    update

--- 18-1-24/flutter_application_1/lib/assinment1.dart
+++ 18-1-24/flutter_application_1/lib/assinment1.dart
@@ -50,7 +50,7 @@ class Assinment1 extends StatelessWidget{
                   ),
                   Image.network(
                     "https://dnm.nflximg.net/api/v6/2DuQlx0fM4wd1nzqm5BFBi6ILa8/AAAAQeIeKt7LlqIJPKrT4aQijclj7K43xRSU3dQXNESNdNbnnJbT6LLWVRT9srUUbHbOo-iOH-8v3o16pUDMQ6tCgNGlkvfwvDOprROIZpQ2rgHtop9rHvbYlvzavMmUSGBCXjynJ80dn4nqZzZmzIUJMQpS.jpg?r=943",
-                      height: 400,
+                      height: 500,
                       width: 200,
                   ),
                   Image.network(
